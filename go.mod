b47b09b3980ff61b738db341bf4d248d2d9e909c
Javed Khan <tuxcanfly@gmail.com>
2023-08-22 06:23:17 -0700

    wip

--- go.mod
+++ go.mod
@@ -2,13 +2,64 @@ module testmount
 
 go 1.20
 
-require github.com/filecoin-project/dagstore v0.7.0
+require github.com/ipld/go-car/v2 v2.4.1
 
 require (
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/ipfs/bbloom v0.0.4 // indirect
+	github.com/ipfs/go-block-format v0.0.3 // indirect
+	github.com/ipfs/go-blockservice v0.5.0 // indirect
+	github.com/ipfs/go-cid v0.3.2 // indirect
+	github.com/ipfs/go-datastore v0.6.0 // indirect
+	github.com/ipfs/go-ipfs-blockstore v1.2.0 // indirect
+	github.com/ipfs/go-ipfs-ds-help v1.1.0 // indirect
+	github.com/ipfs/go-ipfs-exchange-interface v0.2.0 // indirect
+	github.com/ipfs/go-ipfs-util v0.0.2 // indirect
+	github.com/ipfs/go-ipld-cbor v0.0.5 // indirect
+	github.com/ipfs/go-ipld-format v0.3.0 // indirect
+	github.com/ipfs/go-ipld-legacy v0.1.0 // indirect
+	github.com/ipfs/go-log v1.0.5 // indirect
 	github.com/ipfs/go-log/v2 v2.5.1 // indirect
+	github.com/ipfs/go-merkledag v0.8.1 // indirect
+	github.com/ipfs/go-metrics-interface v0.0.1 // indirect
+	github.com/ipfs/go-verifcid v0.0.1 // indirect
+	github.com/ipld/go-codec-dagpb v1.3.1 // indirect
+	github.com/ipld/go-ipld-prime v0.16.0 // indirect
+	github.com/jbenet/goprocess v0.1.4 // indirect
+	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/multiformats/go-base32 v0.0.4 // indirect
+	github.com/multiformats/go-base36 v0.1.0 // indirect
+	github.com/multiformats/go-multibase v0.1.1 // indirect
+	github.com/multiformats/go-multicodec v0.5.0 // indirect
+	github.com/multiformats/go-multihash v0.2.1 // indirect
+	github.com/multiformats/go-varint v0.0.6 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/petar/GoLLRB v0.0.0-20210522233825-ae3b015fd3e9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/polydawn/refmt v0.0.0-20201211092308-30ac6d18308e // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/whyrusleeping/cbor v0.0.0-20171005072247-63513f603b11 // indirect
+	github.com/whyrusleeping/cbor-gen v0.0.0-20200123233031-1cdf64d27158 // indirect
+	go.opentelemetry.io/otel v1.7.0 // indirect
+	go.opentelemetry.io/otel/trace v1.7.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/goleak v1.1.12 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.22.0 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/exp v0.0.0-20210714144626-1041f73d31d8 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	lukechampine.com/blake3 v1.1.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,72 +1,131 @@
 package main
 
 import (
-	"bytes"
-	"crypto/rand"
+	"context"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"os"
 	"sync"
 
-	"github.com/filecoin-project/dagstore/mount"
+	carv2 "github.com/ipld/go-car/v2"
+	"github.com/ipld/go-car/v2/blockstore"
 )
 
-type fileMount struct {
-	buf *bytes.Buffer
-	mount.FileMount
+const src = "DFD7A65CE5EAA57214A0F5F4C976445F34B6E333420E9423A9AEABEAC8A83C1F"
+const dst = "DFD7A65CE5EAA57214A0F5F4C976445F34B6E333420E9423A9AEABEAC8A83C1F.copy"
+
+const cidPrintCount = 5
+
+func openCAR(path string) {
+	// Open a new ReadOnly blockstore from a CARv1 file.
+	// Note, `OpenReadOnly` accepts bot CARv1 and CARv2 formats and transparently generate index
+	// in the background if necessary.
+	// This instance sets ZeroLengthSectionAsEOF option to treat zero sized sections in file as EOF.
+	robs, err := blockstore.OpenReadOnly(path,
+		blockstore.UseWholeCIDs(true),
+		carv2.ZeroLengthSectionAsEOF(true),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer robs.Close()
+
+	// Print root CIDs.
+	roots, err := robs.Roots()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Contains %v root CID(s):\n", len(roots))
+	for _, r := range roots {
+		size, err := robs.GetSize(context.TODO(), r)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("\t%v -> %v\n", r, size)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Print the raw data size for the first 5 CIDs in the CAR file.
+	keysChan, err := robs.AllKeysChan(ctx)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("List of first %v CIDs and their raw data size:\n", cidPrintCount)
+	i := 1
+	for k := range keysChan {
+		if i > cidPrintCount {
+			cancel()
+			break
+		}
+		size, err := robs.GetSize(context.TODO(), k)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("\t%v -> %v bytes\n", k, size)
+		i++
+	}
 }
 
-func (m *fileMount) WriteTo(w io.Writer) (int64, error) {
-	return io.Copy(w, bytes.NewReader(m.buf.Bytes()))
+func LdWrite(w io.Writer, d ...[]byte) error {
+	var sum uint64
+	for _, s := range d {
+		sum += uint64(len(s))
+	}
+
+	buf := make([]byte, 8)
+	n := binary.PutUvarint(buf, sum)
+	_, err := w.Write(buf[:n])
+	if err != nil {
+		return err
+	}
+
+	for _, s := range d {
+		_, err = w.Write(s)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
-func Write(data []byte, mount *fileMount) error {
-    _, err := mount.buf.Write(data)
-    return err
-}
-
-func Put() error {
-    key := "block001.dat"
-	f, err := os.OpenFile(key, os.O_CREATE|os.O_WRONLY, 0600)
+func Put(data []byte) error {
+	f, err := os.OpenFile(dst, os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	// save encoded eds into buffer
-	m := &fileMount{
-		// TODO: buffer could be pre-allocated with capacity calculated based on eds size.
-		buf:       bytes.NewBuffer(nil),
-		FileMount: mount.FileMount{Path: key},
-	}
-	randomBytes := make([]byte, 1024 * 4096)
-	_, err = rand.Read(randomBytes)
+	err = LdWrite(f, data)
 	if err != nil {
-		fmt.Println("Error generating random bytes:", err)
-		return err
-	}
-	err = Write(randomBytes, m)
-	if err != nil {
-		return fmt.Errorf("failed to write to file: %w", err)
-	}
-
-	// write whole buffered mount data in one go to optimize i/o
-	if _, err = m.WriteTo(f); err != nil {
 		return fmt.Errorf("failed to write to file: %w", err)
 	}
 	return nil
 }
 
 func main() {
+    data, err := os.ReadFile(src)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		panic(err)
+	}
+
+	openCAR(src)
+
     var wg sync.WaitGroup
     for i:=0; i < 2; i++ {
         wg.Add(1)
         go func() {
-            if err := Put(); err != nil {
+            if err := Put(data); err != nil {
                 fmt.Println(err)
             }
             wg.Done()
         }()
     }
     wg.Wait()
+
+	openCAR(dst)
 }
